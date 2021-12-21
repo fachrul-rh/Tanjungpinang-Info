a2ff0bd5f7a7ecf27069539b56b58dabc58179cd
Fachrul Rozi Hamonangan Harahap <85205921+fachrul-rh@users.noreply.github.com>
2021-12-21 14:07:35 +0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+/* const firstName = prompt("Siapa namamu?");
+const language = prompt("Bisa berbahasa daerah ?");
+ 
+const user = {
+   name: {
+       first: firstName,
+   },
+   language: language
+};
+ 
+if (user.language === "jawa") {
+   alert("Matur nuwun " + user.name.first + "!");
+} else if (user.language === "sunda") {
+   alert("Hatur nuhun " + user.name.first + "!");
+} else if (user.language === "batak") {
+   alert("Hoorasss " + user.name.first + "!");
+} else {
+   alert("Terimakasih " + user.name.first + "!");
+}
+*/
+
+alert("Terimakasih Sudah Berkunjung");
